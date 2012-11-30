b5c865e25d897d391d5ac3eff1c18e2f34b885cf
Albert Wu <albert12132@gmail.com>
2012-11-30 01:40:13 -0800

    Interface: added exceptions.py

---
+++ interface/exceptions.py
@@ -0,0 +1,17 @@
+
+class JavaException(BaseException):
+    def __init__(self, error_type, msg):
+        assert isinstance(error_type, str), 'Not a valid error type'
+        self.type = error_type
+        self.msg = msg
+
+    def __str__(self):
+        return "{}: {}".format(self.type, self.msg)
+
+class CompileException(JavaException):
+    def __init__(self, msg):
+        super().__init__('CompileException', msg)
+
+class RuntimeException(JavaException):
+    pass
+
